250b6b1f3e5a661befc7210c330b609884d93e75
Milan Damen <github@darvit.nl>
2020-09-25 20:18:58 +0200

    Fixed origin checking

--- main.go
+++ main.go
@@ -64,6 +64,9 @@ func (c *Client) Close() error {
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
 }
 
 func registerWebsocketHandlers(mux *http.ServeMux) {
